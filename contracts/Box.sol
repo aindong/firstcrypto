a4a24522272d735526a2a53fcb81c241012c7437
Alleo Indong <freedom_war550@live.com.ph>
2021-11-24 10:24:01 +0800

    adding auth to our box contract

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,14 +2,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Import Auth from the access-control subdirectory
+import "./access-control/Auth.sol";
+
 contract Box {
     uint256 private _value;
+    Auth private _auth;
 
     // Event for when the value changes.
     event ValueChanged(uint256 value);
 
+    constructor() {
+        _auth = new Auth(msg.sender);
+    }
+
     // Store a new value in the contract
     function store(uint256 value) public {
+        // Require that the sender is authorized to store
+        require(_auth.isAdministrator(msg.sender), "Only administrators can store");
+
         _value = value;
         emit ValueChanged(value);
     }
