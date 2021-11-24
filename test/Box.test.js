5ba2fc81d66b6198255e7c6366e0178a08408ecb
Alleo Indong <freedom_war550@live.com.ph>
2021-11-24 11:37:09 +0800

    atlast all 3 test succeed

--- test/Box.test.js
+++ test/Box.test.js
@@ -2,26 +2,45 @@
 // Load dependencies
 const { expect } = require("chai");
 
+// Import utilities from Test Helpers
+const { BN, expectEvent, expectRevert } = require("@openzeppelin/test-helpers");
+
 // Load compiled artifacts
 const Box = artifacts.require("Box");
 
 // Start test block
-contract("Box", () => {
-  // Define test variables
-  let box;
+contract("Box", ([owner, other]) => {
+  // use large integers
+  const value = new BN("42");
 
   // Define test functions
   beforeEach(async () => {
     // Deploy new instance of Box
-    box = await Box.new();
+    this.box = await Box.new({ from: owner });
   });
 
   // Test case: box.store() and box.retrieve()
   it("should return a value previously stored", async () => {
     // store value
-    await box.store(42);
+    await this.box.store(value, { from: owner });
 
     // Check value
-    expect((await box.retrieve()).toString()).to.equal("42");
+    expect(await this.box.retrieve()).to.be.bignumber.equal(value);
+  });
+
+  it("emites a ValueChanged event after store", async () => {
+    // store value
+    const receipt = await this.box.store(value, { from: owner });
+
+    // Check value
+    expectEvent(receipt, "ValueChanged", { value: value });
+  });
+
+  it("should not allow non owner to store a value", async () => {
+    // store value
+    await expectRevert(
+      this.box.store(value, { from: other }),
+      "Ownable: caller is not the owner"
+    );
   });
 });
